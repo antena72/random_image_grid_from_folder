0c020127d83a5e1bce06a488df631a238a6ec0c5
ximena <ximenamay@hotmail.com>
2019-08-29 15:18:33 -0300

    Add files via upload

---
+++ inc_lee_carpeta.php
@@ -0,0 +1,35 @@
+<?php
+// $carpeta_seleccionada = 'nombre de la carpeta';
+$carpeta_seleccionada = 'pescadosjpg';
+$lista_imagenes = array();
+
+function lista_archivos($carpeta){
+    global $lista_imagenes;
+    $extensiones = array('jpg', 'jpeg', 'png', 'gif', 'bmp');
+    //$carpeta = __DIR__ ."/".$carpeta."/";
+    $carpeta = "../acuario/pescadosjpg/";
+    //lee los archivos de la carpeta
+    $archivos = scandir($carpeta);
+    //pasar todo a minusculas
+    $archivos = array_map('strtolower', $archivos);
+    //itera los elementos de la carpeta y sólo pone en el array las imágenes
+    for ($i=0; $i < count($archivos); $i++) { 
+        if ($archivos[$i] != '.' && $archivos[$i] != '..' && $archivos[$i] != 'anchoita.png') {
+           $archivo = pathinfo($archivos[$i]);
+           $extension = $archivo['extension'];
+           if (in_array($extension, $extensiones)) {
+               array_push($lista_imagenes,$archivos[$i]);
+               
+           }
+        }       
+    }
+    //randomiza orden dentro del array
+    shuffle($lista_imagenes);
+    //convierte a lista a json
+    $lista_imagenes = json_encode($lista_imagenes, JSON_FORCE_OBJECT);
+    
+    echo $lista_imagenes;
+}
+
+lista_archivos($lista_imagenes);
+?>
